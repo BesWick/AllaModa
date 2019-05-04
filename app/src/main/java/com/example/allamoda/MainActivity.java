44ef6746b206080cfaa88f959e30cfeb0fe4c9b7
geoh12 <henryx75@gmail.com>
2019-05-03 19:23:48 -0700

    Added Authentication to home page

--- app/src/main/java/com/example/allamoda/MainActivity.java
+++ app/src/main/java/com/example/allamoda/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.allamoda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 import java.util.Arrays;
@@ -11,7 +13,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+        } else {
+            // not signed in
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                    new AuthUI.IdpConfig.GoogleBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
 
+        }
         setContentView(R.layout.activity_main);
 
 
