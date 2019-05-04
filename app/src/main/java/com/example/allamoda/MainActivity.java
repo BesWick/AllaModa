0484957a9d94e0f7d5e6911a966779521d52054e
geoh12 <henryx75@gmail.com>
2019-05-03 19:09:39 -0700

    Current Stable Version 5/3/19

--- app/src/main/java/com/example/allamoda/MainActivity.java
+++ app/src/main/java/com/example/allamoda/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.allamoda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.firebase.ui.auth.AuthUI;
-import com.google.firebase.auth.FirebaseAuth;
+
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
@@ -11,17 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        if (auth.getCurrentUser() != null) {
-        } else {
-            // not signed in
-            startActivityForResult(
-                    // Get an instance of AuthUI based on the default app
-                    AuthUI.getInstance().createSignInIntentBuilder().build(),
-                    RC_SIGN_IN);
-
-        }
         setContentView(R.layout.activity_main);
+
 
     }
 }
