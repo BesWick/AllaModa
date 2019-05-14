a45256f02f483a95375802b0159e19132987dd64
Alexandra Tutino <alexandratutino@gmail.com>
2019-05-14 10:48:15 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/allamoda/CreateNewOutfit.java
@@ -0,0 +1,13 @@
+package com.example.allamoda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateNewOutfit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_outfit);
+    }
+}
---
+++ app/src/main/java/com/example/allamoda/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.allamoda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private android.widget.Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+    public void openActivity(){
+        Intent intent2 = new Intent(this, Main2Activity.class);
+        startActivity(intent2);
+    }
+}
