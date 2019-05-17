4b2034903c7da0325bd7b343d16b264fe2d686eb
Henry Cheng <henryx75@gmail.com>
2019-05-16 19:50:23 -0700

    Fixed user authentication and removed redundant files

--- app/src/main/java/com/example/allamoda/MainActivity.java
+++ app/src/main/java/com/example/allamoda/MainActivity.java
@@ -3,17 +3,40 @@ package com.example.allamoda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 123;
     private android.widget.Button button;
-
+    private String TAG = "default";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+        } else {
+            // not signed in
+            Log.d(TAG, "onCreate: FIREBASE USER NOT FOUND");
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                    new AuthUI.IdpConfig.EmailBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+            //When new user is created then go to home page
 
+
+        }
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void openActivity(){
-        Intent intent2 = new Intent(this, Login.class);
+        Intent intent2 = new Intent(this, HomePage.class);
         startActivity(intent2);
     }
 }
