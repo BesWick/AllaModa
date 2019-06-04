f69833225e4320845b0d0538d01c63fac02a0c34
John Dobrota <wickthebest@gmail.com>
2019-06-04 02:28:01 -0700

    Added more articles of clothings

--- app/src/main/java/com/example/allamoda/long_sleeve_shirt_list.java
+++ app/src/main/java/com/example/allamoda/long_sleeve_shirt_list.java
@@ -1,13 +1,265 @@
 package com.example.allamoda;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class long_sleeve_shirt_list extends AppCompatActivity {
+import java.io.IOException;
+
+public class long_sleeve_shirt_list extends Activity implements Camera.PictureCallback, SurfaceHolder.Callback {
+
+
+    public static final String EXTRA_CAMERA_DATA = "camera_data";
+
+    private static final String KEY_IS_CAPTURING = "is_capturing";
+
+    private Camera mCamera;
+    private ImageView mCameraImage;
+    private SurfaceView mCameraPreview;
+    private Button mCaptureImageButton;
+    private byte[] mCameraData;
+    private boolean mIsCapturing;
+
+    private View.OnClickListener mCaptureImageButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            captureImage();
+        }
+    };
+
+    private View.OnClickListener mRecaptureImageButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            setupImageCapture();
+        }
+    };
+
+    private View.OnClickListener mDoneButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (mCameraData != null) {
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_CAMERA_DATA, mCameraData);
+                setResult(RESULT_OK, intent);
+            } else {
+                setResult(RESULT_CANCELED);
+            }
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_long_sleeve_shirt_list);
+
+        if (ContextCompat.checkSelfPermission(long_sleeve_shirt_list.this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // here, Permission is not granted
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, 50);
+        }
+
+        ImageView  image = (ImageView) findViewById(R.id.camera_preview);
+        image = (ImageView) findViewById(R.id.camera_preview);
+        image.setAlpha(0.5f);
+
+        mCameraImage = (ImageView) findViewById(R.id.camera_image_view);
+        mCameraImage.setVisibility(View.INVISIBLE);
+
+        mCameraPreview = (SurfaceView) findViewById(R.id.preview_view);
+        final SurfaceHolder surfaceHolder = mCameraPreview.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        mCaptureImageButton = (Button) findViewById(R.id.capture_image_button);
+        mCaptureImageButton.setOnClickListener(mCaptureImageButtonClickListener);
+
+        final Button doneButton = (Button) findViewById(R.id.done_button);
+        doneButton.setOnClickListener(mDoneButtonClickListener);
+
+        mIsCapturing = true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putBoolean(KEY_IS_CAPTURING, mIsCapturing);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        mIsCapturing = savedInstanceState.getBoolean(KEY_IS_CAPTURING, mCameraData == null);
+        if (mCameraData != null) {
+            setupImageDisplay();
+        } else {
+            setupImageCapture();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mCamera == null) {
+            try {
+                mCamera = Camera.open();
+                mCamera.setDisplayOrientation(90);
+                mCamera.setPreviewDisplay(mCameraPreview.getHolder());
+                int angleToRotate=getRoatationAngle(long_sleeve_shirt_list.this, Camera.CameraInfo.CAMERA_FACING_FRONT);
+                mCamera.setDisplayOrientation(angleToRotate);
+                mCamera.setDisplayOrientation(90);
+                if (mIsCapturing) {
+                    mCamera.setDisplayOrientation(90);
+                    mCamera.startPreview();
+
+                }
+            } catch (Exception e) {
+                Toast.makeText(long_sleeve_shirt_list.this, "Unable to open camera.", Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+        mCameraData = data;
+
+        Bitmap orignalImage = BitmapFactory.decodeByteArray(data, 0, data.length);
+        Bitmap bitmapImage = rotate(orignalImage, 0);
+        DBHandler db = new DBHandler();
+        db.addImage(DBHandler.LONG_SHIRT_OPTION, bitmapImage, new DBHandler.MyCallback() {
+            @Override
+            public void onCallback(Bitmap value) {
+
+            }
+        });
+
+        setupImageDisplay();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if (mCamera != null) {
+            try {
+                mCamera.setPreviewDisplay(holder);
+                if (mIsCapturing) {
+                    mCamera.startPreview();
+                }
+            } catch (IOException e) {
+                Toast.makeText(long_sleeve_shirt_list.this, "Unable to start camera preview.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public static Bitmap rotate(Bitmap bitmap, int degree) {
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+        Matrix mtx = new Matrix();
+        //       mtx.postRotate(degree);
+        mtx.setRotate(degree);
+
+        return Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, true);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+    private void captureImage() {
+        mCamera.takePicture(null, null, this);
+    }
+
+    private void setupImageCapture() {
+        mCameraImage.setVisibility(View.INVISIBLE);
+        mCameraPreview.setVisibility(View.VISIBLE);
+        mCamera.startPreview();
+        mCaptureImageButton.setText(R.string.capture_image);
+        mCaptureImageButton.setOnClickListener(mCaptureImageButtonClickListener);
+    }
+
+    private void setupImageDisplay() {
+        Bitmap bitmap = BitmapFactory.decodeByteArray(mCameraData, 0, mCameraData.length);
+        bitmap = rotate(bitmap, 90);
+        mCameraImage.setImageBitmap(bitmap);
+        mCamera.stopPreview();
+        mCameraPreview.setVisibility(View.INVISIBLE);
+        mCameraImage.setVisibility(View.VISIBLE);
+        mCaptureImageButton.setText(R.string.recapture_image);
+        mCaptureImageButton.setOnClickListener(mRecaptureImageButtonClickListener);
+    }
+
+
+
+
+
+    /**
+     * Get Rotation Angle
+     *
+     * @param mContext
+     * @param cameraId
+     *            probably front cam
+     * @return angel to rotate
+     */
+    public static int getRoatationAngle(Activity mContext, int cameraId) {
+        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = mContext.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360; // compensate the mirror
+        } else { // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        return result;
     }
 }
