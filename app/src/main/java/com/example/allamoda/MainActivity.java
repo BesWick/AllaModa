66937b312362e671a3b5d3a51b2ee31855dae277
John Dobrota <wickthebest@gmail.com>
2019-06-03 22:55:49 -0700

    MainActivity Code updated to the right version

--- app/src/main/java/com/example/allamoda/MainActivity.java
+++ app/src/main/java/com/example/allamoda/MainActivity.java
@@ -1,87 +1,133 @@
 package com.example.allamoda;
 
-import android.Manifest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.hardware.Camera;
-import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnCapture;
-    private ImageView imgCapture;
-    Camera camera;
+    private static final int TAKE_PICTURE_REQUEST_B = 100;
 
+    private ImageView mCameraImageView;
+    private Bitmap mCameraBitmap;
+    private Button mSaveImageButton;
 
-    private static final int REQ_CAMERA_IMAGE = 123;
-    private static final int Image_Capture_Code = 1;
+    private OnClickListener mCaptureImageButtonClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startImageCapture();
+        }
+    };
+
+    private OnClickListener mSaveImageButtonClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            File saveFile = openFileForImage();
+            if (saveFile != null) {
+                saveImageToFile(saveFile);
+            } else {
+                Toast.makeText(MainActivity.this, "Unable to open file for saving image.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            // here, Permission is not granted
-            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, 50);
-        }
+        mCameraImageView = (ImageView) findViewById(R.id.camera_image_view);
 
+        findViewById(R.id.capture_image_button).setOnClickListener(mCaptureImageButtonClickListener);
 
-        String message = "Click the button below to start";
-        if(cameraNotDetected()){
-            message = "No camera detected, clicking the button below will have unexpected behaviour.";
-        }
-        TextView cameraDescriptionTextView = (TextView) findViewById(R.id.text_view_camera_description);
-        cameraDescriptionTextView.setText(message);
+        mSaveImageButton = (Button) findViewById(R.id.save_image_button);
+        mSaveImageButton.setOnClickListener(mSaveImageButtonClickListener);
+        mSaveImageButton.setEnabled(false);
     }
 
-
-
-
-private boolean cameraNotDetected() {
-        return !getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
-        }
-
-
-    public void onUseCameraClick(View button){
-        Intent intent = new Intent(this, CameraActivity.class);
-        startActivityForResult(intent, REQ_CAMERA_IMAGE);
-    }
-
-
-
-
-@Override
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        byte [] bp;
-        Bitmap bmp;
-
-        if (requestCode == REQ_CAMERA_IMAGE) {
+        if (requestCode == TAKE_PICTURE_REQUEST_B) {
             if (resultCode == RESULT_OK) {
-                bp =  data.getByteArrayExtra("data");
-                bmp = BitmapFactory.decodeByteArray(bp, 0, bp.length);
-                imgCapture = findViewById(R.id.image_view_captured_image);
-                imgCapture.setImageBitmap(bmp);
-            } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                // Recycle the previous bitmap.
+                if (mCameraBitmap != null) {
+                    mCameraBitmap.recycle();
+                    mCameraBitmap = null;
+                }
+                Bundle extras = data.getExtras();
+                mCameraBitmap = (Bitmap) extras.get("data");
+                byte[] cameraData = extras.getByteArray(CameraActivity.EXTRA_CAMERA_DATA);
+                if (cameraData != null) {
+                    mCameraBitmap = BitmapFactory.decodeByteArray(cameraData, 0, cameraData.length);
+                    mCameraImageView.setImageBitmap(mCameraBitmap);
+                    mSaveImageButton.setEnabled(true);
+                }
+            } else {
+                mCameraBitmap = null;
+                mSaveImageButton.setEnabled(false);
             }
         }
     }
 
+    private void startImageCapture() {
+        startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), TAKE_PICTURE_REQUEST_B);
+        startActivityForResult(new Intent(MainActivity.this, CameraActivity.class), TAKE_PICTURE_REQUEST_B);
+    }
 
+    private File openFileForImage() {
+        File imageDirectory = null;
+        String storageState = Environment.getExternalStorageState();
+        if (storageState.equals(Environment.MEDIA_MOUNTED)) {
+            imageDirectory = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    "com.oreillyschool.android2.camera");
+            if (!imageDirectory.exists() && !imageDirectory.mkdirs()) {
+                imageDirectory = null;
+            } else {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_mm_dd_hh_mm",
+                        Locale.getDefault());
 
+                return new File(imageDirectory.getPath() +
+                        File.separator + "image_" +
+                        dateFormat.format(new Date()) + ".png");
+            }
+        }
+        return null;
+    }
 
-
-
-
+    private void saveImageToFile(File file) {
+        if (mCameraBitmap != null) {
+            FileOutputStream outStream = null;
+            try {
+                outStream = new FileOutputStream(file);
+                if (!mCameraBitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)) {
+                    Toast.makeText(MainActivity.this, "Unable to save image to file.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Saved image to: " + file.getPath(),
+                            Toast.LENGTH_LONG).show();
+                }
+                outStream.close();
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Unable to save image to file.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
